ac6dd0700912cd83512b1d5fdcfb366bea76b727
ci010 <cijhn@hotmail.com>
2015-12-02 17:51:14 -0800

    Now it handles the width/height  and position

--- src/main/java/net/ci010/minecrafthelper/gui/GuiComponent.java
+++ src/main/java/net/ci010/minecrafthelper/gui/GuiComponent.java
@@ -7,6 +7,8 @@ import net.minecraft.client.gui.Gui;
  */
 public abstract class GuiComponent extends Gui implements Drawable
 {
+	protected int x, y;
+
 	public Type type()
 	{
 		return Type.back;
@@ -16,4 +18,31 @@ public abstract class GuiComponent extends Gui implements Drawable
 	{
 		front, back
 	}
+
+	public int getX()
+	{
+		return x;
+	}
+
+	public int getY()
+	{
+		return y;
+	}
+
+	public abstract int getWidth();
+
+	public abstract int getHeight();
+
+	/**
+	 * button: 1-left, 2-middle, 3-right
+	 */
+	interface MouseListener
+	{
+		void actionPerform(int x, int y, int button);
+	}
+
+	public interface MouseClick extends MouseListener {}
+
+	public interface MouseRelease extends MouseListener {}
+
 }
