0f71b60f09f2cb570af0d5e688b92a26512e9ec2
ci010 <cijhn@hotmail.com>
2015-12-06 00:59:08 -0800

    Make it fit for the GuiComponent now.

--- src/main/java/net/ci010/minecrafthelper/gui/GuiString.java
+++ src/main/java/net/ci010/minecrafthelper/gui/GuiString.java
@@ -1,6 +1,7 @@
 package net.ci010.minecrafthelper.gui;
 
-import net.minecraft.client.Minecraft;
+import net.ci010.minecrafthelper.util.GuiUtil;
+import org.lwjgl.Sys;
 
 /**
  * @author ci010
@@ -8,13 +9,14 @@ import net.minecraft.client.Minecraft;
 public class GuiString extends GuiComponent
 {
 	private String string;
-	private int x, y, color;
+	private int x, y, width = 0, height, color;
 
 	public GuiString(String string, int x, int y)
 	{
 		this.string = string;
 		this.x = x;
 		this.y = y;
+		this.height = 8;
 		this.color = 10526880;//TODO see what the black is
 	}
 
@@ -31,8 +33,34 @@ public class GuiString extends GuiComponent
 	}
 
 	@Override
+	public int getWidth()
+	{
+		return width;
+	}
+
+	@Override
+	public int getHeight()
+	{
+		return height;
+	}
+
+	@Override
 	public void draw()
 	{
-		this.drawString(Minecraft.getMinecraft().fontRendererObj, string, x, y, color);
+		if (this.width == 0)
+		{
+			if (GuiUtil.font() == null)
+			{
+				System.out.println("WTF");
+				return;
+			}
+			if (this.string == null)
+			{
+				System.out.println("holy shit");
+				return;
+
+			}
+		}
+		this.drawString(GuiUtil.font(), string, x, y, color);
 	}
 }
