ac487f8ad0ae56a999f05b7c8f1e879144dac9f1
ci010 <cijhn@hotmail.com>
2015-12-19 00:14:34 -0800

    Add javadoc. Add a method for switching elements.

--- src/main/java/net/ci010/minecrafthelper/util/ArrayUtil.java
+++ src/main/java/net/ci010/minecrafthelper/util/ArrayUtil.java
@@ -5,15 +5,37 @@ package net.ci010.minecrafthelper.util;
  */
 public class ArrayUtil
 {
+	/**
+	 * Concat the second array to the first array.
+	 *
+	 * @param arr  The first array.
+	 * @param arr2 The second array.
+	 * @param <T>  The type of these array.
+	 */
 	public static <T> void concat(T[] arr, T[] arr2)
 	{
 		int oldLength = arr.length;
 		int length = oldLength + arr.length;
-		arr = (T[]) new Object[length];
+		arr = GenericUtil.cast(new Object[length]);
 		int i;
 		for (i = 0; i < oldLength; ++i)
 			arr[i] = arr[i];
 		for (; i < length; ++i)
 			arr[i] = arr2[i - oldLength];
+	}
+
+	/**
+	 * Switch two element in an array.
+	 *
+	 * @param arr    The array will be handled.
+	 * @param first  The first element position.
+	 * @param second The second element position.
+	 * @param <T>    The type of the array.
+	 */
+	public static <T> void switchElement(T[] arr, int first, int second)
+	{
+		T temp = arr[first];
+		arr[first] = arr[second];
+		arr[second] = temp;
 	}
 }
