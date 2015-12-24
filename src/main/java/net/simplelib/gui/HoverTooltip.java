35f734f4d20a096202fa62b9f44b44bfe471840d
ci010 <cijhn@hotmail.com>
2015-12-23 19:29:20 -0800

    C

---
+++ src/main/java/net/simplelib/gui/HoverTooltip.java
@@ -0,0 +1,26 @@
+package net.simplelib.gui;
+
+/**
+ * @author ci010
+ */
+public abstract class HoverTooltip implements MouseProperty.Hover
+{
+	private GuiBorderTexts texts;
+
+	@Override
+	public void onHover(int mouseX, int mouseY)
+	{
+		if (texts == null)
+			texts = getTextLines();
+		texts.setPos(mouseX, mouseY);
+		texts.draw();
+	}
+
+	@Override
+	public float delay()
+	{
+		return 1.5f;
+	}
+
+	public abstract GuiBorderTexts getTextLines();
+}
