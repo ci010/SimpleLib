cc64fc1031f00cf0cef5bd9bb9d230f764a60a5b
ci010 <cijhn@hotmail.com>
2016-01-10 01:50:57 -0800

    Add support for the key registry.

---
+++ src/main/java/net/simplelib/common/registry/KeyBindingHandler.java
@@ -0,0 +1,33 @@
+package net.simplelib.common.registry;
+
+import com.google.common.collect.ImmutableList;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.fml.common.gameevent.InputEvent;
+import net.simplelib.common.registry.annotation.type.Handler;
+
+import java.util.Collection;
+
+/**
+ * @author ci010
+ */
+@Handler
+public class KeyBindingHandler
+{
+	private static int size;
+	private static ImmutableList<KeyPair> pairs;
+
+	public static void buildList(Collection<KeyPair> in)
+	{
+		pairs = ImmutableList.copyOf(in);
+		size = pairs.size();
+	}
+
+	@SubscribeEvent
+	public void onKeyDown(InputEvent.KeyInputEvent event)
+	{
+		KeyPair pair;
+		for (int i = 0; i < size; ++i)
+			if ((pair = pairs.get(i)).getKeyBinding().isPressed())
+				pair.onKeyPressed();
+	}
+}
---
+++ src/main/java/net/simplelib/common/registry/delegate/KeyRegisterDelegate.java
@@ -0,0 +1,68 @@
+package net.simplelib.common.registry.delegate;
+
+import com.google.common.collect.Lists;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import net.simplelib.common.Local;
+import net.simplelib.common.registry.KeyBindingHandler;
+import net.simplelib.common.registry.KeyPair;
+import net.simplelib.common.registry.abstracts.ASMRegistryDelegate;
+import net.simplelib.common.registry.abstracts.KeyHandler;
+import net.simplelib.common.registry.annotation.type.ASMDelegate;
+import net.simplelib.common.registry.annotation.type.Key;
+import net.simplelib.common.utils.GenericUtil;
+
+import java.util.List;
+
+/**
+ * @author ci010
+ */
+@ASMDelegate
+public class KeyRegisterDelegate extends ASMRegistryDelegate<Key>
+{
+	private List<KeyPair> pairs = Lists.newArrayList();
+
+	@Mod.EventHandler
+	public void preInit(FMLPreInitializationEvent event)
+	{
+		Key anno = this.getAnnotation();
+		Class<?> clz = this.getAnnotatedClass();
+		if (!KeyHandler.class.isAssignableFrom(clz))
+		{
+			//// TODO: 2016/1/5 Log
+			return;
+		}
+		else
+		{
+			try
+			{
+				final KeyHandler handler = GenericUtil.cast(this.getAnnotatedClass().newInstance());
+				pairs.add(new KeyPair(Local.translate("key.".concat(anno.id()).concat(".description"), anno.id()),
+						anno.keyCode(),
+						Local.translate("key.".concat(anno.id()).concat(".category"), anno.id()))
+				{
+					@Override
+					public void onKeyPressed()
+					{
+						handler.onKeyPressed(this.getKeyBinding());
+					}
+				});
+			}
+			catch (InstantiationException e)
+			{
+				e.printStackTrace();
+			}
+			catch (IllegalAccessException e)
+			{
+				e.printStackTrace();
+			}
+		}
+	}
+
+	@Mod.EventHandler
+	public void postInit(FMLPostInitializationEvent event)
+	{
+		KeyBindingHandler.buildList(pairs);
+	}
+}
