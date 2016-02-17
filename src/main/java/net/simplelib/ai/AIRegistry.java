8adb1676ec96d9fef9d79455c42cdb40a8afe987
ci010 <cijhn@hotmail.com>
2016-02-16 20:51:55 -0800

    Implement AI

--- src/main/java/net/simplelib/ai/AIRegistry.java
+++ src/main/java/net/simplelib/ai/AIRegistry.java
@@ -1,12 +1,13 @@
 package net.simplelib.ai;
 
+import api.simplelib.ai.AIHandler;
 import com.google.common.collect.HashMultimap;
 import com.google.common.collect.Multimap;
 import net.minecraft.entity.EntityLiving;
 import net.minecraftforge.event.entity.EntityJoinWorldEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
-import net.simplelib.common.registry.annotation.type.ModHandler;
-import net.simplelib.common.utils.GenericUtil;
+import api.simplelib.common.ModHandler;
+import api.simplelib.utils.GenericUtil;
 
 import java.util.Collection;
 
@@ -14,7 +15,7 @@ import java.util.Collection;
 public class AIRegistry
 {
 	private static Multimap<Class<? extends EntityLiving>, AIHandler> handlerMultimap = HashMultimap.create();
-	private static AIHandler.AIManager cache = new AIHandler.AIManager();
+	private static AIManagerImpl cache = new AIManagerImpl();
 
 	public static void registerHandler(AIHandler handler)
 	{
