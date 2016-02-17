c53a62b9cff48236b792293c89f3463cc0e654f1
ci010 <cijhn@hotmail.com>
2016-02-16 20:52:30 -0800

    Create client stop event.

---
+++ src/main/java/net/simplelib/event/ClientStopEvent.java
@@ -0,0 +1,10 @@
+package net.simplelib.event;
+
+import net.minecraftforge.fml.common.eventhandler.Event;
+
+/**
+ * @author ci010
+ */
+public class ClientStopEvent extends Event
+{
+}
---
+++ src/main/java/net/simplelib/event/ClientStopPoster.java
@@ -0,0 +1,21 @@
+package net.simplelib.event;
+
+import net.minecraft.client.gui.GuiMainMenu;
+import net.minecraftforge.client.event.GuiScreenEvent;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import api.simplelib.common.ModHandler;
+
+/**
+ * @author ci010
+ */
+@ModHandler
+public class ClientStopPoster
+{
+	@SubscribeEvent
+	public void onStop(GuiScreenEvent.ActionPerformedEvent event)
+	{
+		if (event.gui instanceof GuiMainMenu && event.button.id == 4)
+			MinecraftForge.EVENT_BUS.post(new ClientStopEvent());
+	}
+}
