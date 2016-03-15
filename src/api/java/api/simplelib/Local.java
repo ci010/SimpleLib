844cebe3b5ee69cfbd631bda8bcb1042463d60a0
ci010 <cijhn@hotmail.com>
2016-03-14 18:29:15 -0700

    Support the ChatComponent

--- src/api/java/api/simplelib/Local.java
+++ src/api/java/api/simplelib/Local.java
@@ -1,5 +1,8 @@
 package api.simplelib;
 
+import net.minecraft.util.ChatComponentText;
+import net.minecraft.util.ChatComponentTranslation;
+import net.minecraft.util.IChatComponent;
 import net.minecraft.util.StatCollector;
 import net.simplelib.common.registry.LanguageReporter;
 
@@ -8,16 +11,21 @@ import net.simplelib.common.registry.LanguageReporter;
  */
 public class Local
 {
-	public static String translate(String id)
+	public static String trans(String id)
 	{
-		return translate(id, id);
+		return trans(id, StatCollector.translateToFallback(id));
 	}
 
-	public static String translate(String id, String fallback)
+	public static String trans(String id, String fallback)
 	{
 		if (StatCollector.canTranslate(id))
 			return StatCollector.translateToLocal(id);
 		LanguageReporter.instance().report(id);
 		return fallback;
+	}
+
+	public static IChatComponent newChat(String id)
+	{
+		return new ChatComponentText(trans(id));
 	}
 }
