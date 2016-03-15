5e4514740efa549fd33e9359e6cb9f292eef0e78
ci010 <cijhn@hotmail.com>
2016-03-14 18:43:54 -0700

    Add alot helpful functions.

--- src/main/java/net/simplelib/common/RegionalCache.java
+++ src/main/java/net/simplelib/common/RegionalCache.java
@@ -21,6 +21,9 @@ public class RegionalCache<Position>
 		this.radius = radius;
 	}
 
+	public int getRadius()
+	{return radius;}
+
 	public float getCenter()
 	{
 		return this.cache[radius][radius];//TODO check this
@@ -33,22 +36,36 @@ public class RegionalCache<Position>
 		return temp;
 	}
 
+	public int getDiameter()
+	{
+		return diameter;
+	}
+
 	public void moveTo(Position pos)
 	{
+		if (this.center == null)
+		{
+			this.refresh(pos);
+			return;
+		}
 		int xOffset = mapping.getXOffset(this.center, pos);
 		int zOffset = mapping.getZOffset(this.center, pos);
+		if (xOffset == 0 && zOffset == 0)
+			return;
 		this.center = pos;
+
 		this.move(xOffset, zOffset);
 	}
 
 	private void move(int xOffset, int zOffset)
 	{
 		if (xOffset > diameter || zOffset > diameter)
-			this.refresh();
+			this.refresh(this.center);
 		else
 		{
 			float[][] temp = new float[diameter][diameter];
-			int range = this.diameter - xOffset;
+
+			int range = this.diameter + xOffset;
 			if (xOffset < 0)
 				for (int x = 0; x < range; x++)
 					if (zOffset >= 0)
@@ -66,18 +83,45 @@ public class RegionalCache<Position>
 				{
 					float v = temp[x][z];
 					if (v == 0.0F)
+					{
 						temp[x][z] = this.delegate.calculate(
 								this.mapping.getPosFromOffset(this.center, x - this.radius, z - this.radius));
+					}
 				}
 			this.cache = temp;
 		}
 	}
 
-	public void refresh()
+	public float getMean()
 	{
+		float sum = 0f;
+		for (int x = 0; x < this.cache.length; x++)
+			for (int z = 0; z < cache[x].length; z++)
+				sum += this.cache[x][z];
+		return sum / (this.diameter * this.diameter);
+	}
+
+	public void refresh(Position center)
+	{
+		this.center = center;
 		for (int x = 0; x < this.diameter; x++)
 			for (int z = 0; z < this.diameter; z++)
-				this.delegate.calculate(this.mapping.getPosFromOffset(this.center, x - radius, z - radius));
+				this.cache[x][z] = this.delegate.calculate(this.mapping.getPosFromOffset(this.center, x - radius, z - radius));
+	}
+
+	@Override
+	public String toString()
+	{
+		StringBuilder builder = new StringBuilder();
+		for (int x = 0; x < this.diameter; x++)
+		{
+			for (int y = 0; y < this.diameter; y++)
+			{
+				builder.append(cache[x][y]).append(" ");
+			}
+			builder.append("\n");
+		}
+		return builder.toString();
 	}
 
 	public interface ValueDelegate<T>
