1129bdf5c595ae48fe984af1fe46a411095591d4
ci010 <cijhn@hotmail.com>
2016-03-14 18:24:33 -0700

    Raise an exception and handle the nick name

--- src/api/java/api/simplelib/command/ISimpleCommand.java
+++ src/api/java/api/simplelib/command/ISimpleCommand.java
@@ -1,6 +1,8 @@
 package api.simplelib.command;
 
+import net.minecraft.command.CommandException;
 import net.minecraft.command.ICommandSender;
+import net.minecraft.command.WrongUsageException;
 
 /**
  * @author ci010
@@ -9,5 +11,23 @@ public interface ISimpleCommand
 {
 	String name();
 
-	void processCommand(ICommandSender sender, String[] args);
+	void processCommand(ICommandSender sender, String[] args) throws CommandException;
+
+	interface NickName
+	{
+		String[] getNikeName();
+	}
+
+	class UsageException extends WrongUsageException
+	{
+		public UsageException(String cmdName)
+		{
+			this(cmdName, new Object[0]);
+		}
+
+		public UsageException(String cmdName, Object... replacements)
+		{
+			super("commands.".concat(cmdName).concat(".usage"), replacements);
+		}
+	}
 }
--- src/api/java/api/simplelib/command/ModCommand.java
+++ src/api/java/api/simplelib/command/ModCommand.java
@@ -15,5 +15,4 @@ import java.lang.annotation.Target;
 @Target(value =
 		{ElementType.TYPE})
 public @interface ModCommand
-{
-}
+{}
